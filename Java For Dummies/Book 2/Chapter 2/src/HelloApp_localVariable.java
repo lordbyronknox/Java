0934a351ae9adee936a4b835c07ec3edd405b2ef
lordbyron <byronknox@yahoo.co.uk>
2022-07-12 11:03:21 +0200

    Chapter 1

---
+++ Java For Dummies/Book 2/Chapter 2/src/HelloApp_localVariable.java
@@ -0,0 +1,22 @@
+//Local variable: Declared in the body of a method.  No 'static' keyword.
+//Can only be use within the method. (other methods in the class can't see it.)
+//Only exist when the method is being used.
+
+public class HelloApp_localVariable {
+    
+    public static void main(String[] args) {
+        
+        String helloMessage;                    //local variable: must be declared before it is used.
+        helloMessage = "Hello, World!";         //...usually at the start of the method's body.
+                                                
+        System.out.println(helloMessage);
+        
+        
+// this would not work as the variable is declared after it is used.
+/*      helloMessage = "Hello World";
+        System.out.println(helloMessage);
+        String helloMessage;
+*/
+        
+    }
+}
