f846e87b82ca790536fa4d5814846e6509730d49
lordbyron <byronknox@yahoo.co.uk>
2022-07-22 14:11:54 +0200

    book 3 update

--- Java For Dummies/Book 3/Book_3/src/Chapter_3/Statics.java
+++ Java For Dummies/Book 3/Book_3/src/Chapter_3/Statics.java
@@ -1,7 +1,20 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+STATICS
+
+A static method is a method that isn't associated with an instance of a class.
+Instead, the method belongs to the class itself. This means you can access a static 
+field or method without creating an instance of a class.
+
+You access a static field or method by referencing the class name, not the variable that
+references an object.
+
+
+
+
+
+
+
+*/
 package Chapter_3;
 
 /**
