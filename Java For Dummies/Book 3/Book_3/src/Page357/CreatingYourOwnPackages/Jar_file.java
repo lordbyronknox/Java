f9bdb3c2bf59ed852ab9c51f3e57e825e96e01d2
lordbyron <byronknox@yahoo.co.uk>
2022-08-08 08:38:03 +0200

    archiving a package

--- Java For Dummies/Book 3/Book_3/src/Page357/CreatingYourOwnPackages/Jar_file.java
+++ Java For Dummies/Book 3/Book_3/src/Page357/CreatingYourOwnPackages/Jar_file.java
@@ -1,7 +1,31 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+JAR FILES   [JAR = Java Archive]
+- a single compressed file containing one or more classes
+- The entire java API is stored in a single JAR file named 'rt.java'.
+- They are created by the jar utility, which is in the Java bin directory.
+- It contains a MANIFEST FILE that contains info about the files in the archive.
+- JAR files are the normal way to distribute finished java applications, and class libraries.
+
+Basic fotmat of jar command:
+    jar options jar-file [manifest-file] class-files...
+
+
+ARCHIVING A PACKAGE
+1. Open a command line in the package's root.
+2. Use a jar command that specifies:
+    - the option cf
+    - the name of the jar file
+    - and the path to the class files you want to archive.
+      eg:   jar cf utils.jar com\lowewriter\ util\*.class
+3. Verify the jar file was created properly using commands that specifies the
+    option 'tf', and name of jar file. This lists the content of the jar file.
+    eg.     jar tf utils.jar
+
+
+
+
+
+*/
 package Page357.CreatingYourOwnPackages;
 
 /**
