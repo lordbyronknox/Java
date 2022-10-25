da8bb1911a837830114792e06a97c418e38e4db2
lordbyron <byronknox@yahoo.co.uk>
2022-10-25 10:42:21 +0200

    Geeks4Geeks

---
+++ labs/Java_Advanced/11-Exceptions/practices/practice1/com.geeks/src/com/geeks/ComGeeks.java
@@ -0,0 +1,15 @@
+
+package com.geeks;
+
+
+public class ComGeeks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Base b = new Derived();
+        b.show();
+    }
+    
+}
